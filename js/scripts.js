a981605491b2a96abcbdc03695133a32e9a832f9
Arianna Chau <ariannachau@yahoo.com>
2017-02-23 17:41:07 -0800

    polish communication between HTML and js, finish result screens.

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,31 @@ $(document).ready(function() {
   $("form.quiz").submit(function(event) {
     event.preventDefault();
     var answer1 = parseInt($('input:radio[name=question1]:checked').val());
-    console.log("Answer 1 is: " + answer1);
+    // console.log("Answer 1 is: " + answer1);
     var answer2 = parseInt($('input:radio[name=question2]:checked').val());
-    console.log("Answer 2 is: " + answer2)
+    // console.log("Answer 2 is: " + answer2)
     var answer3 = parseInt($('input:radio[name=question3]:checked').val());
-    console.log("Answer 3 is: " + answer3)
+    // console.log("Answer 3 is: " + answer3)
     var answer4 = parseInt($('input:radio[name=question4]:checked').val());
-    console.log("Answer 4 is: " + answer4)
+    // console.log("Answer 4 is: " + answer4)
     var answer5 = parseInt($('input:radio[name=question5]:checked').val());
-    console.log("Answer 5 is: " + answer5)
+    // console.log("Answer 5 is: " + answer5)
     var userScore = (answer1 + answer2 + answer3 + answer4 + answer5)
+    var firstNameInput = $("input#firstName").val();
+
+    $('.firstName').text(firstNameInput);
 
     $("#result").show();
 
     if (userScore === 5) {
         $("#borabora").show();
 
-    } else {
+    }  else if (userScore === 10) {
         $("#norway").show();
+
+    }  else if (userScore === 15) {
+        $("#newyork").show();
+
     }
 
   });
